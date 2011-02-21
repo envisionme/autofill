ce1e498d545b486e7850d979e0251ac7862bf964
Rui Ferreira <rui@madnez-ag.co.za>
2011-02-21 12:49:46 +0200

    Check if the user add text in field or not.

---
+++ js/autofill.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+		$('#edit-field-test-0-value').autofill({
+			value: 'e.g. http://www.example.com',
+			defaultTextColor: '#666',
+			activeTextColor: '#333',
+			prePopulate: 'http://'
+		});
+});
