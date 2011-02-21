f38dd71f163941e70a9d70679c529577abbd9014
Rui Ferreira <rui@madnez-ag.co.za>
2011-02-21 15:52:14 +0200

    Romve any unnecessary code

--- js/autofill.js
+++ js/autofill.js
@@ -1,8 +1,10 @@
-$(document).ready(function(){
-		$('#edit-field-test-0-value').autofill({
+Drupal.behaviors.autofill = function(context) {
+		
+		$('#edit-field-test-0-value').autofill( {
 			value: 'e.g. http://www.example.com',
 			defaultTextColor: '#666',
-			activeTextColor: '#333',
+			activeTextColor: '#333' ,
 			prePopulate: 'http://'
 		});
-});
+		
+ }
