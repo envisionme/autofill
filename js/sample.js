d724ac6540ee002d463da720009c1062901418d7
Rui Ferreira <ferreira.rcb@gmail.com>
2011-12-06 11:24:02 +0200

    Issue #1179906 by patrickd: Fix code style issues

--- js/sample.js
+++ js/sample.js
@@ -1,23 +1,23 @@
-Drupal.behaviors.autofill = function(context) { 
- $("#edit-field-test-0-value").autofill( {
-			value: "Hallo world",
-			prePopulate: "Hallo Piet",
-			defaultTextColor: "#666666",
-			activeTextColor: "#000000",
-	});
-		
- $("#any_field_ID").autofill( {
-			value: "Hallo world",
-			prePopulate: "Hallo Piet",
-			defaultTextColor: "#666666",
-			activeTextColor: "#000000",
-	});
-	
-	$("#any_field_ID").autofill( {
-			value: "Hallo world",
-			prePopulate: "Hallo Piet",
-			defaultTextColor: "#666666",
-			activeTextColor: "#000000",
-	});
-	
+Drupal.behaviors.autofill = function(context) {
+$("#edit-field-test-0-value").autofill( {
+ value: "Hallo world",
+ prePopulate: "Hallo Piet",
+ defaultTextColor: "#666666",
+ activeTextColor: "#000000",
+});
+
+$("#any_field_ID").autofill( {
+ value: "Hallo world",
+ prePopulate: "Hallo Piet",
+ defaultTextColor: "#666666",
+ activeTextColor: "#000000",
+});
+
+$("#any_field_ID").autofill( {
+ value: "Hallo world",
+ prePopulate: "Hallo Piet",
+ defaultTextColor: "#666666",
+ activeTextColor: "#000000",
+});
+
 }
