abd19996ab6f5b2c1389549df781186904198443
Rui Ferreira <ferreira.rcb@gmail.com>
2011-12-06 12:41:55 +0200

    Fix code styling errors

--- js/sample.js
+++ js/sample.js
@@ -3,20 +3,20 @@ Drupal.behaviors.autofill = function(context) {
    value: "Hallo world",
    prePopulate: "Hallo Piet",
    defaultTextColor: "#666666",
-   activeTextColor: "#000000",
+   activeTextColor: "#000000"
   });
 
   $("#any_field_ID").autofill( {
    value: "Hallo world",
    prePopulate: "Hallo Piet",
    defaultTextColor: "#666666",
-   activeTextColor: "#000000",
+   activeTextColor: "#000000"
   });
 
   $("#any_field_ID").autofill( {
    value: "Hallo world",
    prePopulate: "Hallo Piet",
    defaultTextColor: "#666666",
-   activeTextColor: "#000000",
+   activeTextColor: "#000000"
   });
 }
